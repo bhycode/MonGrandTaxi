a16116939b32b943bd36861af2c64bcde02d6f62
bhycode <badreddynelhirch2@gmail.com>
2024-02-13 10:23:15 +0100

    Add footer.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -141,8 +141,17 @@
     </div>
 </div>
 
+<!-- Footer Section -->
+<footer class="bg-dark text-light py-4">
+    <div class="container text-center">
+        <p>&copy; 2024 MonGrandTaxi. All rights reserved.</p>
+    </div>
+</footer>
+
+
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
+
